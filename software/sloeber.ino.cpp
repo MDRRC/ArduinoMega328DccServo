448723d198f461bbe36d8adbb8813e9ec5a9673f
Robert Evers <evers4202000@yahoo.com>
2022-04-17 21:12:22 +0200

    Renamed directory.

--- software/sloeber.ino.cpp
+++ software/sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-04-17 20:55:31
+//This file has been generated on 2022-04-17 21:06:12
 
 #include "Arduino.h"
 #define NUMSERVOS 8
