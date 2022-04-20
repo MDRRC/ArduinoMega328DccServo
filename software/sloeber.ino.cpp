0c9abc4e95c217ff0e5a81bea0cb00e735846128
Robert Evers <evers4202000@yahoo.com>
2022-04-20 20:47:01 +0200

    * Added cyclic read of CV's. When changed set new min/max value and activate servo to new position.  * Added comments headers.

--- software/sloeber.ino.cpp
+++ software/sloeber.ino.cpp
@@ -2,19 +2,16 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2022-04-17 21:06:12
+//This file has been generated on 2022-04-20 20:39:58
 
 #include "Arduino.h"
-#define NUMSERVOS 8
-#define SERVOSPEED 50
-#define SERVO_DETACH_CNT 1000 / SERVOSPEED
-#define RUN_LED_TIME 1000
-#define RUN_LED_PIN 4
-#include <DCC_Decoder.h>
+#include <NmraDcc.h>
 #include <Servo.h>
 
-void BasicAccDecoderPacket_Handler(int address, boolean activate, byte data) ;
+void notifyCVResetFactoryDefault() ;
+void notifyDccAccState(uint16_t Addr, uint16_t BoardAddr, uint8_t OutputAddr, uint8_t State) ;
 void RunLed() ;
+void CheckForCvUpdate() ;
 void setup() ;
 void loop() ;
 
